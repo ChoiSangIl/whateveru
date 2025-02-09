992fa7f8d7dd99cf643be96d711367b281fbd03c
Choi Sang Il <wo566@naver.com>
2025-02-09 22:09:35 +0900

    study: future #1

--- reactive-streams/src/main/kotlin/JavaCompletableFuture.kt
+++ reactive-streams/src/main/kotlin/JavaCompletableFuture.kt
@@ -8,6 +8,18 @@ fun getFuture(): Future<String> {
     val executor = Executors.newSingleThreadExecutor()
     return try {
          executor.submit<String> {
+             println("getFuture 실행됨")
+             "Hello, World!"
+        }
+    } finally {
+        executor.shutdown()
+    }
+}
+
+fun getFutureAfter1s(): Future<String> {
+    val executor = Executors.newSingleThreadExecutor()
+    return try {
+        executor.submit<String> {
             Thread.sleep(1000)
             "Hello, World!"
         }
@@ -17,6 +29,7 @@ fun getFuture(): Future<String> {
 }
 
 fun main(){
-    val helloWorld: String = getFuture().get()
-    println(helloWorld)
+    val future: Future<String> = getFuture()
+    // future 의 상태 조회
+    println("future-isDone : ${future.isDone} future-isCancelled : ${future.isCancelled}")
 }
