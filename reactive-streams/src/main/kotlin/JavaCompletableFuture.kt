ee182cce3cb53ba946b2d5019f5ff79c5b7345b6
Choi Sang Il <wo566@naver.com>
2025-03-20 22:59:47 +0900

    study: future

--- reactive-streams/src/main/kotlin/JavaCompletableFuture.kt
+++ reactive-streams/src/main/kotlin/JavaCompletableFuture.kt
@@ -3,12 +3,14 @@ package org.example
 
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
+import java.util.concurrent.RejectedExecutionException
 
 fun getFuture(): Future<String> {
     val executor = Executors.newSingleThreadExecutor()
     return try {
          executor.submit<String> {
              println("getFuture 실행됨")
+             Thread.sleep(100)
              "Hello, World!"
         }
     } finally {
@@ -28,8 +30,28 @@ fun getFutureAfter1s(): Future<String> {
     }
 }
 
+fun getFutureException(): Future<String> {
+    val executor = Executors.newSingleThreadExecutor()
+    return try {
+        executor.submit<String> {
+            throw IllegalArgumentException("Error")
+        }
+    } finally {
+        executor.shutdown()
+    }
+}
+
 fun main(){
-    val future: Future<String> = getFuture()
+    val future1: Future<String> = getFutureAfter1s()
+    val future2: Future<String> = getFutureAfter1s()
+    val future3: Future<String> = getFutureAfter1s()
+
     // future 의 상태 조회
-    println("future-isDone : ${future.isDone} future-isCancelled : ${future.isCancelled}")
+    future1.cancel(true)
+    println("future-isDone : ${future1.isDone} future-isCancelled : ${future1.isCancelled}")
+    println("future-isDone : ${future2.isDone} future-isCancelled : ${future2.isCancelled}")
+    println("future-isDone : ${future3.isDone} future-isCancelled : ${future3.isCancelled}")
+
+    val futureException = getFutureException()
+    println("futureException future-isDone : ${futureException.isDone} future-isCancelled : ${futureException.isCancelled}")
 }
