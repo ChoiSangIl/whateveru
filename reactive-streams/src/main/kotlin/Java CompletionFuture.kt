5731485f8078b19446272b43f3eea66cf8712568
Choi Sang Il <wo566@naver.com>
2025-03-31 20:45:04 +0900

    study: CompletionStage... 시작

---
+++ reactive-streams/src/main/kotlin/Java CompletionFuture.kt
@@ -0,0 +1,10 @@
+package org.example
+
+import java.util.concurrent.CompletionStage
+
+/**
+ * CompletionStage Interface
+ * @see CompletionStage
+ */
+class CompletionFutureStudy(
+)
--- reactive-streams/src/main/kotlin/Java Future.kt
+++ reactive-streams/src/main/kotlin/Java Future.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 import java.util.concurrent.Executors
 import java.util.concurrent.Future
 
-class FutureTest {
+class JavaFutureStudy {
     private fun helloFuture(): Future<String> {
         val executor = Executors.newSingleThreadExecutor()
         return try {
