3028444d92c19dc5d1a0ecd9d47e0f0924639d44
Choi Sang Il <wo566@naver.com>
2025-04-01 21:54:49 +0900

    study: runningStage > thenAccept & thenAcceptAsync

--- reactive-streams/src/main/kotlin/Java CompletionFuture.kt
+++ reactive-streams/src/main/kotlin/Java CompletionFuture.kt
@@ -21,13 +21,18 @@ class CompletionFutureStudy{
     }
 
     @Test
-    @DisplayName("thenAccept 는 FUTURE STAGE 가 DONE 상태이면 메인 THREAD 에서 실행 됨. finishedStage 블락되는 작업이 함께 있으면 메인 스레드가 블락될 수 있음.")
+    @DisplayName(
+        """
+            thenAccept 는 FUTURE STAGE 가 DONE 상태이면 메인 THREAD 에서 실행 됨. finishedStage 블락되는 작업이 함께 있으면 메인 스레드가 블락될 수 있음. 
+            비동기 작업이 빨리 끝났는데 Block 되는 작업이 있다면 main thread 가 블락킹 될 수 있음 > Blocking 비동기 상태가 된다
+        """
+    )
     fun thenAcceptFinishedStageTest(){
         printlnWithThreadName("start main")
 
         finishedStage()
-            .thenAccept(::printlnWithThreadName)
-            .thenAccept(::printlnWithThreadName)
+            .thenAccept{ printlnWithThreadName("thenAccept >> $it") }
+            .thenAccept{ printlnWithThreadName("thenAccept2 >> $it") }
 
         printlnWithThreadName("after thenAccept")
 
@@ -41,8 +46,8 @@ class CompletionFutureStudy{
         println("[${Thread.currentThread().name}] start main")
 
         finishedStage()
-            .thenAcceptAsync(::printlnWithThreadName)
-            .thenAcceptAsync(::printlnWithThreadName)
+            .thenAcceptAsync{ printlnWithThreadName("thenAcceptAsync >> $it") }
+            .thenAcceptAsync{ printlnWithThreadName("thenAcceptAsync2 >> $it") }
 
         printlnWithThreadName("after thenAccept")
 
@@ -57,4 +62,31 @@ class CompletionFutureStudy{
         }
     }
 
+    @Test
+    @DisplayName("thenAccept 는 FUTURE STAGE 가 RUNNING 상태이면 별도의 Thread 에서 실행 됨")
+    fun thenAcceptRunningStageTest(){
+        printlnWithThreadName("start main")
+
+        runningStage()
+            .thenAccept{ printlnWithThreadName("thenAccept >> $it") }
+            .thenAccept{ printlnWithThreadName("thenAccept2 >> $it") }
+
+        printlnWithThreadName("after thenAccept")
+
+        Thread.sleep(2000)
+    }
+
+    @Test
+    @DisplayName("thenAcceptAsync 는 FUTURE STAGE 가 RUNNING 상태이면 별도의 Thread 에서 실행 됨")
+    fun thenAcceptAsyncRunningStageTest(){
+        printlnWithThreadName("start main")
+
+        runningStage()
+            .thenAcceptAsync{ printlnWithThreadName("thenAcceptAsync >> $it") }
+            .thenAcceptAsync{ printlnWithThreadName("thenAcceptAsync2 >> $it") }
+
+        printlnWithThreadName("after thenAccept")
+
+        Thread.sleep(2000)
+    }
 }
