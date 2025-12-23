ee7d9f37e140ac35ab9740dc7c7a09147ca663db
Choi Sang Il <wo566@naver.com>
2025-12-23 19:41:26 +0900

    spring-ai study start

---
+++ spring-ai/src/main/kotlin/whatever/study/SpringAiApplication.kt
@@ -0,0 +1,11 @@
+package whatever.study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringAiApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringAiApplication>(*args)
+}
---
+++ spring-ai/src/test/kotlin/whatever/study/SpringAiApplicationTests.kt
@@ -0,0 +1,13 @@
+package whatever.study
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringAiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
